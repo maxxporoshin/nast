b290868611676d37a37abe245e394aca35de524c
maxxporoshin <maxxporoshin@gmail.com>
2016-12-09 04:15:48 +0300

    chpok

--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@ import time
 import itertools
 import math
 from itertools import combinations
+#from graph_tool.all import *
 
 
 def generate_graph(n, p):
@@ -13,16 +14,21 @@ def generate_graph(n, p):
             if random.random() < p:
                 G[i][j] = G[j][i] = 1
     return G
+path = []
+full = []
+used = []
+flag = False
 
-def rebuild(path, full):
-    print "Path " + str(path)
+def rebuild():
+    global path, full, used, flag
+    print "rebuild" + str(path)
     full_old = copy.deepcopy(full)
 
     full[path[0]] = [path[1]]
     full[path[1]] = [path[0], path[2]]
     full[path[2]] = [path[1]]
 
-    for i in xrange(len(path[3:-1])):
+    for i in xrange(3, len(path[:-1])):
         l = path[i-1]
         v = path[i]
         r = path[i+1]
@@ -39,51 +45,62 @@ def rebuild(path, full):
 
 
 
-def find_path(G, v, path, full, flag):
-    if flag[0]:
+def find_path(G, v):
+    global path, full, used, flag
+    if flag:
         return
-    if full[v] and len(full[v]) == 1 and full[v][1] != path[-2]:
-        path.append(full[v][1])
-        find_path(G, full[v][1], path, flag)
+    if full[v] and len(full[v]) == 1 and full[v][0] != path[-2]:
+        path.append(full[v][0])
+        used[full[v][0]] = True
+        find_path(G, full[v][0])
+        if not flag:
+            used[full[v][0]] = False
+            path = path[:-1]
         return
     if full[v] and len(full[v]) == 1:
         for to in xrange(len(G)):
-            if G[v][to] == 1:
+            if G[v][to] == 1 and not used[to]:
                 if not full[to]:
                     path.append(to)
-                    flag[0] = True
+                    flag = True
                     return
                 path.append(to)
-                find_path(G, to, path, flag)
-                if flag[0]:
+                used[to] = True
+                find_path(G, to)
+                if flag:
                     return
                 path = path[:-1]
-        return 
-    if full[v] and len(full[v]) == 2:
+                used[to] = False
+        return
+    if full[v] and len(full[v]) == 2 and path[-2] in full[v]:
         for to in xrange(len(G)):
-            if G[v][to] == 1 and to != path[-2]:
+            if G[v][to] == 1 and to != path[-2] and not used[to]:
                 if not full[to]:
                     path.append(to)
                     flag[0] = True
                     return
                 path.append(to)
-                find_path(G, to, path, flag)
-                if flag[0]:
+                used[to] = True
+                find_path(G, to)
+                if flag:
                     return
+                used[to] = False
                 path = path[:-1]
         return
-    
+
+
 def Kun3(G):
+    global path, full, used, flag
     n = len(G)
     full = [None for x in xrange(n)] # pair, 1 elem, or None
     vertices = list(xrange(n))
-    
+
     for v in xrange(n):
         print "V + "+ str(v) + " Full + " + str(full)
         if full[v]:
             continue
         for pair in combinations(vertices, 2):
-            if G[v][pair[0]] == 1 and G [pair[1]][v] == 1 and not full[pair[0]] and not full[pair[1]]:
+            if G[v][pair[0]] == 1 and G[pair[1]][v] == 1 and not full[pair[0]] and not full[pair[1]]:
                 full[pair[0]] = [v]
                 full[pair[1]] = [v]
                 full[v] = [pair[0], pair[1]]
@@ -93,16 +110,26 @@ def Kun3(G):
         for pair in combinations(vertices, 2):
             if G[v][pair[0]] == 1 and G[pair[1]][v] == 1 and full[pair[0]] and not full[pair[1]]:
                 path = [pair[1], v, pair[0]]
-                find_path(G, pair[0], path, full, [False])
-                rebuild(path, full)
+                used = [False] * n
+                used[pair[1]] = True
+                used[v] = True
+                used[pair[0]] = True
+                flag = False
+                find_path(G, pair[0])
+                rebuild()
                 break
         if full[v]:
             continue
         for pair in combinations(vertices, 2):
             if G[v][pair[0]] == 1 and G[pair[1]][v] == 1 and not full[pair[0]] and full[pair[1]]:
                 path = [pair[0], v, pair[1]]
-                find_path(G, pair[1], path, full, [False])
-                rebuild(path, full)
+                used = [False] * n
+                used[pair[1]] = True
+                used[v] = True
+                used[pair[0]] = True
+                flag = False
+                find_path(G, pair[1])
+                rebuild()
                 break
 
     solution = []
@@ -111,10 +138,18 @@ def Kun3(G):
             solution.append([v] + full[v])
     return solution
 
+
+"""
+
+g = Graph()
+g.add_vertex()
+graph_draw(g, vertex_text=g.vertex_index, vertex_font_size=18,
+           output_size=(200, 200), output="two-nodes.png")
+"""
+
 random.seed(12345)
 G = generate_graph(10, 0.5)
 print G
 #G = [[0, 1, 1], [1, 0, 0], [1, 0, 0]]
 s = Kun3(G)
 print s
-    
---
+++ chpok.py
@@ -0,0 +1,119 @@
+import random
+import copy
+import time
+import itertools
+import math
+
+
+def generate_graph(n, p):
+    G = [[0 for j in range(n)] for i in range(n)]
+    for i in range(n):
+        for j in range(i + 1, n):
+            if random.random() < p:
+                G[i][j] = G[j][i] = 1
+    return G
+
+
+def greedy(G, k, vertices=[]):
+    n = len(G)
+    if not vertices:
+        vertices = list(range(n))
+    chains = []
+    vertices_to_try = vertices[:]
+    while vertices_to_try:
+        v = vertices_to_try[0]
+        found = False
+        chain, stack = [], [(v, [v])]
+        while not found and stack:
+            i, path = stack.pop()
+            for j in range(n):
+                if G[i][j] == 1 and j in vertices and j not in path:
+                    if len(path) == k - 1:
+                        found = True
+                        chain = path + [j]
+                        break
+                    stack.append((j, path + [j]))
+        if chain:
+            chains.append(chain)
+            for l in chain:
+                vertices.remove(l)
+                if l in vertices_to_try:
+                    vertices_to_try.remove(l)
+        else:
+            vertices_to_try.remove(v)
+    return chains
+
+
+def local_search(S, G, k):
+    solution = copy.deepcopy(S)
+    iterations = 0
+    more_chains = True
+    while more_chains:
+        more_chains = False
+        for i in range(len(solution)):
+            T = copy.deepcopy(solution)
+            T.pop(i)
+            vertices = set(range(len(G)))
+            for chain in T:
+                vertices = vertices - set(chain)
+            new_chains = greedy(G, k, list(vertices))
+            if len(new_chains) > 1:
+                solution = T + new_chains
+                more_chains = True
+                break
+        iterations += 1
+    print('Iterations: ' + str(iterations))
+    return solution
+
+
+def simulated_annealing(S, G, k, p1, p2):
+        solution = copy.deepcopy(S)
+        iterations = 0
+        more_chains = True
+        while more_chains:
+            more_chains = False
+            for i in range(len(solution)):
+                T = copy.deepcopy(solution)
+                T.pop(i)
+                vertices = set(range(len(G)))
+                for chain in T:
+                    vertices = vertices - set(chain)
+                new_chains = greedy(G, k, list(vertices))
+                if len(new_chains) > 1 or random.random() < math.exp(-p1 * iterations):
+                    solution = T + new_chains
+                    more_chains = True
+                    break
+            if not more_chains:
+                subsets = itertools.combinations(range(len(solution)), 2)
+                for subset in subsets:
+                    T = copy.deepcopy(solution)
+                    T.pop(subset[1])
+                    T.pop(subset[0])
+                    vertices = set(range(len(G)))
+                    for chain in T:
+                        vertices = vertices - set(chain)
+                    new_chains = greedy(G, k, list(vertices))
+                    if len(new_chains) > 2 or random.random() < math.exp(-p2 * iterations):
+                        solution = T + new_chains
+                        more_chains = True
+                        break
+            iterations += 1
+        print('Iterations: ' + str(iterations))
+        return solution
+
+
+G = generate_graph(100, 0.1)
+k = 3
+print('Greedy:')
+t = time.time()
+greedy_solution = greedy(G, k)
+print(greedy_solution)
+print('{0:e}'.format(time.time() - t))
+print('Local search:')
+t = time.time()
+print(len(local_search(greedy_solution, G, k)))
+print('{0:e}'.format(time.time() - t))
+print('Simulated annealing:')
+t = time.time()
+print(len(simulated_annealing(greedy_solution, G, k, 0.5, 0.5)))
+print('{0:e}'.format(time.time() - t))
